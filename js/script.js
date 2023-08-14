34a5667ea82d22a87f14ef28669f69032b08936d
Halil Can Parlayan <69989315+CanParlayan@users.noreply.github.com>
2023-08-14 11:51:16 +0300

    Bazı değişiklikler
    -Animasyon eklenecek fotoğraflar eklenecek
    fa-fa icons çalışmıyor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Delay the fade-in effect slightly for a smoother transition
     setTimeout(function () {
         document.querySelector(".slideCall").classList.add("show");
         document.querySelector(".slideEmail").classList.add("show");
